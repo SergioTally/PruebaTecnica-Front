43c6dff0d988f2374637b4ec3b709c8900c1a815
sergiotally <sergiotally@gmail.com>
2023-03-27 15:10:40 -0600

    Mostrar informacion de Fiscalias en el Swiper

--- src/Fiscalias.js
+++ src/Fiscalias.js
@@ -1,5 +1,10 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { FreeMode } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Fiscalias(){
   const url="http://localhost:8080/MP/allFiscalia"
@@ -16,22 +21,61 @@ function Fiscalias(){
   },[])
 
   
-      return (
-        <div className="App">
-          <header className="App-header">
-          <button className='Pregunta SinColor'>
-         {!todos ? 'Cargando...': todos.map((todo,index)=>{ 
-            return <p key={index}>
-                {todo['fs_id']},
-                {todo['fs_nombre']},
-                {todo['fs_direccion']},
-                {todo['fs_numero']}
-                </p> }) 
-                }
-         </button>
-        </header>
-        </div>
-      );
+    if(!todos)
+    {
+        return (
+            <div className="App">
+              <header className="App-header">
+              <h2>Fiscalias</h2>
+              <h2>Sin Fiscalias que mostrar</h2>
+              <button className='Pregunta'>Agregar Fiscalia</button>
+             <div className='containet py-4 px-4 justify-content-center'></div>
+            </header>
+            </div>
+          );
+    }
+    else
+    {
+        return (
+            <div className="App">
+              <header className="App-header">
+              <h1>Fiscalias</h1>
+             <div className='container py-4 px-4 justify-content-center'>
+                <Swiper
+                    freeMode={true}
+                    grabCursor={true}
+                    modules={[FreeMode]}
+                    className="mySwiper"
+                    slidesPerView={1}
+                >
+                    {todos.map((todo,index)=>{ 
+                return <SwiperSlide>
+                <div className='p-0 overflow-hidden h-100 shadow'>
+                    <div className='overflow-hidden rounded p-0 bs-light'>
+                        <div className='row'>
+                            <label className='col-lg-2'>Nombre</label>
+                            <input className='col-lg-10' readOnly id='fs_nombre' name='fs_nombre' value={todo['fs_nombre']}></input>
+                        </div>
+                        <div className='row'>
+                            <label className='col-lg-2'>Direccion</label>
+                            <input className='col-lg-10' readOnly id='fs_direccion' name='fs_direccion' value={todo['fs_direccion']}></input>
+                        </div>
+                        <div className='row'>
+                            <label className='col-lg-2'>Numero</label>
+                            <input className='col-lg-10' readOnly id='fs_numero' name='fs_numero' value={todo['fs_numero']}></input>
+                        </div>
+                    </div>
+                </div>
+                <h3>{todo['fs_nombre']}</h3>
+            </SwiperSlide>  }) 
+                    }
+                </Swiper>
+             </div>
+            </header>
+            </div>
+          );
+    }
+
       
 }
 
